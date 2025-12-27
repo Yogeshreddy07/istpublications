56ee493b64cd460e26a697f2ce0fb18dc4e46d1d
Yogesh Reddy <yogiyogeshreddy1234@gmail.com>
2025-12-27 16:30:21 +0530

    backend added

--- ist-publications-backend/istpublications_backend/urls.py
+++ ist-publications-backend/istpublications_backend/urls.py
@@ -19,7 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('contact.urls')),  # All contact URLs under /api/
+    path("admin/", admin.site.urls),
+    path("api/", include("contact.urls")),
 ]
+
 
