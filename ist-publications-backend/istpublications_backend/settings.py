944dcce59e8cb1dd91e47b999493ec944ea6e435
Yogesh Reddy <yogiyogeshreddy1234@gmail.com>
2026-01-04 10:34:43 +0530

    Fix 415 multipart parser

--- ist-publications-backend/istpublications_backend/settings.py
+++ ist-publications-backend/istpublications_backend/settings.py
@@ -157,15 +157,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
     ],
-    
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': [
-        'rest_framework.filters.SearchFilter',
-        'rest_framework.filters.OrderingFilter',
-    ],
-} 
+}
+ 
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
